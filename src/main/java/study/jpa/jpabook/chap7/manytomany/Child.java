d06986156d35d1b9b917bf86ac9cad7dd5faf5c9
JJung0 <junghwan__@naver.com>
2025-11-26 10:53:39 +0900

    [FEAT] 다대다 조인 테이블 매핑

---
+++ src/main/java/study/jpa/jpabook/chap7/manytomany/Child.java
@@ -0,0 +1,17 @@
+package study.jpa.jpabook.chap7.manytomany;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Child {
+
+    @Id @GeneratedValue
+    @Column(name = "CHILD_ID")
+    private Long id;
+
+    private String name;
+
+}
