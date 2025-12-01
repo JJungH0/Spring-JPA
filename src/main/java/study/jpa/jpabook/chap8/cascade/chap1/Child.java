26feda01de45ec7ddc655adcd97819909deb4ea0
JJung0 <junghwan__@naver.com>
2025-12-01 14:47:30 +0900

    [FEAT] Cascade 활

--- src/main/java/study/jpa/jpabook/chap8/cascade/chap1/Child.java
+++ src/main/java/study/jpa/jpabook/chap8/cascade/chap1/Child.java
@@ -1,16 +1,18 @@
-package study.jpa.jpabook.chap8.lazyloading2;
+package study.jpa.jpabook.chap8.cascade.chap1;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
-//@Entity
+@Entity
 @Getter @Setter
-public class Team {
-
+public class Child {
     @Id @GeneratedValue
     private Long id;
 
+    @ManyToOne
+    private Parent parent;
 }
